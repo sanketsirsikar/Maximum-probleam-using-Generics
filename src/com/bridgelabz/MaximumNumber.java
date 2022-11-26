7a3030970036a4d21b17495162a6d29aac4429ea
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-11-27 00:31:38 +0530

    uc1 maximum number added

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -1,47 +1,26 @@
 package com.bridgelabz;
-public class MaximumNumber <T extends Comparable> {
-    T variable1;
-    T variable2;
-    T variable3;
-    public MaximumNumber(T variable1, T variable2, T variable3){
-        this.variable1 = variable1;
-        this.variable2 = variable2;
-        this.variable3 = variable3;
-
-    }
-    public void findMax(){
-        MaximumNumber.findMax(this.variable1, this.variable2, this.variable3);
-    }
+public class MaximumNumber {
     public static void main(String[] args) {
         System.out.println("Welcome to the Generics Problems");
-        System.out.println("\n Test the Maximum Three Numbers ");
+        System.out.println("\n Test the Maximum Three Numbers");
         Integer firstNumber = 12;
         Integer secondNumber = 15;
         Integer thirdNumber = 18;
-        Float floatNumber1 = 10.2f;
-        Float floatNumber2 = 18.80f;
-        Float floatNumber3 = 15.70f;
-        String string1 = "Apple";
-        String string2 = "Banana";
-        String string3 = "Peach";
-
         findMax(firstNumber,secondNumber,thirdNumber);
-        findMax(floatNumber1, floatNumber2, floatNumber3);
-        findMax(string1, string2, string3);
     }
-    static<E extends Comparable<E>> void findMax(E firstVariable,E secondVariable,E thirdVariable ){
-        E maximumVariable;
+    static void findMax(Integer firstNumber,Integer secondNumber,Integer thirdNumber ){
+        Integer maximumNumber;
 
-        if(firstVariable.compareTo(secondVariable)>0 && firstVariable.compareTo(thirdVariable)>0) {
-            maximumVariable = firstVariable;
+        if(firstNumber.compareTo(secondNumber)>0 && firstNumber.compareTo(thirdNumber)>0) {
+            maximumNumber = firstNumber;
         }
-        else if(secondVariable.compareTo(firstVariable)>0 && secondVariable.compareTo(thirdVariable)>0) {
-            maximumVariable = secondVariable;
+        else if(secondNumber.compareTo(firstNumber)>0 && secondNumber.compareTo(thirdNumber)>0) {
+            maximumNumber = secondNumber;
         }
         else {
-            maximumVariable = thirdVariable;
+            maximumNumber = thirdNumber;
         }
-        System.out.println(maximumVariable + " is max out of "+firstVariable+", "+secondVariable+" and "+thirdVariable);
+        System.out.println(maximumNumber + " is max out of "+firstNumber+", "+secondNumber+" and "+thirdNumber);
     }
 
 }
